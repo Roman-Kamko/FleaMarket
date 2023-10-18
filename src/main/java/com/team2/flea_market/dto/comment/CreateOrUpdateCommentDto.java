426c1cd4189a70eb09a084a50261dc402dfe9093
Roman-Kamko <r.kamko@mail.ru>
2023-10-18 15:26:47 +0400

    исправление dto

--- src/main/java/com/team2/flea_market/dto/comment/CreateOrUpdateCommentDto.java
+++ src/main/java/com/team2/flea_market/dto/comment/CreateOrUpdateCommentDto.java
@@ -1,15 +1,15 @@
 package com.team2.flea_market.dto.comment;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 @Schema(name = "CreateOrUpdateComment")
 public record CreateOrUpdateCommentDto(
-        @Schema(description = "текст комментария")
-        @Min(8)
-        @Max(64)
+        @Length(min = 8, max = 64, message = "некорректная длина текста комментария: ${validatedValue}")
+        @Schema(description = "текст комментария", minimum = "8", maximum = "64", example = "текст комментария")
         String text
 ) {
 }
