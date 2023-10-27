fdee4a07f06357b22463c658bcfedd36d622427d
Roman-Kamko <r.kamko@mail.ru>
2023-10-27 19:51:02 +0400

    сущность User

---
+++ src/main/java/com/team2/flea_market/entity/User.java
@@ -0,0 +1,49 @@
+package com.team2.flea_market.entity;
+
+import com.team2.flea_market.dto.user.Role;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString(exclude = "ads")
+@EqualsAndHashCode(exclude = "ads")
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "image")
+    private String image;
+
+    @OneToMany(mappedBy = "user")
+    private List<Ad> ads;
+
+}
