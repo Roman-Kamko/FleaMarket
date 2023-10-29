ef4839f8e0844cc21c20a178b6112203936849b4
nikita <7jay94@mail.ru>
2023-10-29 17:37:40 +0200

    added: добавил комментарии к методам.

--- src/main/java/com/team2/flea_market/mapper/CommentMapper.java
+++ src/main/java/com/team2/flea_market/mapper/CommentMapper.java
@@ -13,14 +13,33 @@ import org.springframework.stereotype.Component;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CommentMapper {
 
+    /**
+     * Используй для маппинг в {@link Comment}
+     *
+     * @param commentDto {@link CommentDto}
+     * @return {@link Comment}
+     */
     @Mapping(target = "id", source = "pk")
     @Mapping(target = "user.id", source = "author")
     @Mapping(target = "user.firstName", source = "authorFirstName")
     @Mapping(target = "user.image", source = "authorImage")
     Comment toEntity(CommentDto commentDto);
 
+    /**
+     * Используй для маппинг в {@link Comment} при апдейте
+     *
+     * @param updateCommentDto {@link CreateOrUpdateCommentDto}
+     * @param comment {@link Comment}
+     * @return {@link Comment}
+     */
     Comment toUpdatedOrCreateComment(CreateOrUpdateCommentDto updateCommentDto, @MappingTarget Comment comment);
 
+    /**
+     * Используй для маппинг в {@link CommentDto}
+     *
+     * @param comment {@link Comment}
+     * @return {@link Comment}
+     */
     @Mapping(target = "pk", source = "id")
     @Mapping(target = "author", source = "user.id")
     @Mapping(target = "authorFirstName", source = "user.firstName")
